0a9f4d19c257928e53c3db78a2a3d60f41116c35
adelkaamarenka <adelka.dilaj@gmail.com>
2022-03-11 15:47:42 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ displayedFact }}</h1>
+  <button @click="incrementId()">New fact</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      id: 0,
+      facts: [],
+      factsText: [],
+    };
+  },
+  methods: {
+    async fetchFacts() {
+      const res = await fetch("https://cat-fact.herokuapp.com/facts");
+      this.facts = await res.json();
+      this.factsText = this.facts.map((fact) => fact.text);
+    },
+    incrementId() {
+      if (this.id < this.factsText.length - 1) {
+        this.id++;
+      } else {
+        this.id = 0;
+      }
+    },
+  },
+  mounted() {
+    this.fetchFacts();
+  },
+  computed: {
+    displayedFact() {
+      return this.factsText[this.id];
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
